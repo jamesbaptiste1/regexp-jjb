47ab3de8989b32bf256efacae8577962bf2834f0
James Baptiste <Jamesbaptiste@lewisu.edu>
2020-01-26 15:21:54 -0600

    commit to check azure

--- app.js
+++ app.js
@@ -1,26 +1,38 @@
-// const str = "table football";
+// // const str = "table football";
 
-// const regex = RegExp("foo*");
-// const globalRegex = RegExp("foo*", "g");
+// // const regex = RegExp("foo*");
+// // const globalRegex = RegExp("foo*", "g");
 
-//var input2 = document.getElementById("input2");
+// //var input2 = document.getElementById("input2");
+// function print() {
+//   var demo = (document.getElementById("demo").innerHTML = 5 + 6);
+// }
+// console.log(regex.test(str));
+// // expected output: true
+
+// console.log(regex.test(str));
+// // expected output: true
+
+// console.log(globalRegex.lastIndex);
+// // expected output: 0
+
+// console.log(globalRegex.test(str));
+// // expected output: true
+
+// console.log(globalRegex.lastIndex);
+// // expected output: 9
+
+// console.log(globalRegex.test(str));
+// // expected output: false
+
 function print() {
-  var demo = (document.getElementById("demo").innerHTML = 5 + 6);
+  // var str = "table football";
+  var str = document.getElementById("input1").value;
+
+  //var re = /foo/i;
+  var re = document.getElementById("re");
+  console.log("input1");
+
+  var answer = document.getElementById("answer");
+  console.log((answer.innerHTML = re.test(str)));
 }
-console.log(regex.test(str));
-// expected output: true
-
-console.log(regex.test(str));
-// expected output: true
-
-console.log(globalRegex.lastIndex);
-// expected output: 0
-
-console.log(globalRegex.test(str));
-// expected output: true
-
-console.log(globalRegex.lastIndex);
-// expected output: 9
-
-console.log(globalRegex.test(str));
-// expected output: false
